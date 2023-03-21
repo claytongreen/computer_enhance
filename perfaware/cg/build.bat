c9e1a3594992f575d43c0ac210dced2a8b45f541
Clayton Green <claytron.g@gmail.com>
2023-03-20 22:41:49 -0700

    Add support for a couple push/pop/xchg

--- perfaware/cg/build.bat
+++ perfaware/cg/build.bat
@@ -1,8 +1,19 @@
 @echo off
+setlocal enabledelayedexpansion
+
+where /Q cl.exe || (
+   set __VSCMD_ARG_NO_LOGO=1
+   for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
+   if "!VS!" equ "" (
+     echo ERROR: Visual Studio installation not found
+     exit /b 1
+   )
+   call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
+)
 
 IF NOT EXIST build mkdir build
 pushd build
 
-call cl /nologo /Zi ..\main.cpp /Fedisasm.exe
+call cl /nologo /Zi /FC ..\main.cpp /Fedisasm.exe
 
 popd
--- perfaware/cg/main.cpp
+++ perfaware/cg/main.cpp
@@ -8,8 +8,6 @@
 
 #define assert(Cond) do { if (!(Cond)) __debugbreak(); } while (0)
 
-// TODO: Instead of using assert, have an error flag so we can break and then print out what we've disassembled and then the error
-
 // MEMORY ---------------------------------------------------------------------
 static size_t global_memory_size = 4096 * 8;
 static uint8_t* global_memory_base;
@@ -20,7 +18,7 @@ static uint8_t* global_memory;
   assert(((size_t)(global_memory - global_memory_base) < global_memory_size) && "OUT OF MEMORY")
 // MEMORY ---------------------------------------------------------------------
 
-#include "string.h"
+#include "string.cpp"
 
 // OS -------------------------------------------------------------------------
 static string_t read_entire_file(char* filename) {
@@ -76,6 +74,9 @@ enum operand_t {
   OPERAND_LOOPNZ,
   OPERAND_LOOPZ,
   OPERAND_MOV,
+  OPERAND_XCHG,
+  OPERAND_POP,
+  OPERAND_PUSH,
   OPERAND_SUB,
   // ---------------------
   OPERAND_COUNT,
@@ -108,6 +109,9 @@ static string_t operand_names[OPERAND_COUNT] = {
   STRING_LIT("loopnz"),
   STRING_LIT("loopz"),
   STRING_LIT("mov"),
+  STRING_LIT("xchg"),
+  STRING_LIT("pop"),
+  STRING_LIT("push"),
   STRING_LIT("sub"),
 };
 
@@ -130,6 +134,8 @@ enum register_t {
   REGISTER_BP,
   REGISTER_SI,
   REGISTER_DI,
+  REGISTER_CS,
+  REGISTER_DS,
   // --------------
   REGISTER_COUNT,
 };
@@ -152,6 +158,8 @@ static string_t register_names[REGISTER_COUNT] = {
   STRING_LIT("bp"),
   STRING_LIT("si"),
   STRING_LIT("di"),
+  STRING_LIT("cs"),
+  STRING_LIT("ds"),
 };
 
 enum effective_address_t {
@@ -179,6 +187,11 @@ static string_t address_names[EFFECTIVE_ADDRESS_COUNT] = {
   register_names[REGISTER_BP],
   register_names[REGISTER_BX],
 };
+
+
+static string_t get_register_name(uint8_t reg, uint8_t w) {
+  return register_names[(w * 8) + (reg + 1)];
+}
 
 static uint8_t next_byte(string_t* instruction_stream) {
   assert(instruction_stream->length > 0 && "Unexpected end of instruction stream");
@@ -225,7 +238,7 @@ static string_t next_effective_address(string_t *instruction_stream, uint8_t mod
 }
 
 static string_t next_register_memory(string_t *instruction_stream, uint8_t w, uint8_t mod, uint8_t rm) {
-  string_t result = (mod == 3) ? register_names[(w * 8) + rm + 1] : next_effective_address(instruction_stream, mod, rm);
+  string_t result = (mod == 3) ? get_register_name(rm, w) : next_effective_address(instruction_stream, mod, rm);
   return result;
 }
 
@@ -333,7 +346,7 @@ int main(int argc, char** argv) {
             break;
         }
 
-        string_t register_name = register_names[w * 8 + (reg + 1)];
+        string_t register_name = get_register_name(reg, w);
 
         dest    = d ? register_name : register_memory_name;
         source  = d ? register_memory_name : register_name;
@@ -380,6 +393,83 @@ int main(int argc, char** argv) {
         source = string_pushf("%d", data);
       } break;
 
+      case 0x0E: // PUSH CS
+      {
+        operand = OPERAND_PUSH;
+        dest = register_names[REGISTER_CS];
+      } break;
+
+      case 0x50: // PUSH AX
+      case 0x51: // PUSH CX
+      case 0x52: // PUSH DX
+      case 0x53: // PUSH BX
+      case 0x54: // PUSH SP
+      case 0x55: // PUSH BP
+      case 0x56: // PUSH SI
+      case 0x57: // PUSH DI
+      {
+        register_t reg = (register_t)((int)REGISTER_AX + (b1 - 0x50));
+
+        operand = OPERAND_PUSH;
+        dest = register_names[reg];
+      } break;
+
+      case 0x1E: // PUSH DS
+      {
+        operand = OPERAND_PUSH;
+        dest = register_names[REGISTER_DS];
+      } break;
+
+      case 0x1F: // POP DS
+      {
+        operand = OPERAND_POP;
+        dest = register_names[REGISTER_DS];
+      } break;
+
+      case 0x58: // POP AX
+      case 0x59: // POP CX
+      case 0x5A: // POP DX
+      case 0x5B: // POP BX
+      case 0x5C: // POP SP
+      case 0x5D: // POP BP
+      case 0x5E: // POP SI
+      case 0x5F: // POP DI
+      {
+        register_t reg = (register_t)((int)REGISTER_AX + (b1 - 0x58));
+
+        operand = OPERAND_POP;
+        dest = register_names[reg];
+      } break;
+
+      case 0x8F: // POP r/m
+      {
+        uint8_t b2 = next_byte(&instruction_stream);
+        uint8_t mod = (b2 >> 6);
+        // uint8_t reg = (b2 >> 3) & 7;
+        uint8_t rm  = (b2 >> 0) & 7;
+
+        string_t address = next_effective_address(&instruction_stream, mod, rm);
+
+        operand = OPERAND_POP;
+        dest = string_pushf("word %.*s", STRING_FMT(address));
+      } break;
+
+      case 0x87: // XCHG w,r/m
+      {
+        uint8_t w = (b1 >> 0) & 1;
+
+        uint8_t b2 = next_byte(&instruction_stream);
+        uint8_t mod = (b2 >> 6);
+        uint8_t reg = (b2 >> 3) & 7;
+        uint8_t rm  = (b2 >> 0) & 7;
+
+        string_t address = next_effective_address(&instruction_stream, mod, rm);
+
+        operand = OPERAND_XCHG;
+        dest = get_register_name(reg, w);
+        source = address;
+      } break;
+
       case 0x70: // JO
       case 0x71: // JNO
       case 0x72: // JB/JNAE
@@ -421,6 +511,7 @@ int main(int argc, char** argv) {
           error = STRING_LIT("Too many labels");
         }
 
+        // TODO: Is there a nice way to map this?
         switch(b1) {
           case 0x70: operand = OPERAND_JO;     break;
           case 0x71: operand = OPERAND_JNO;    break;
@@ -474,12 +565,17 @@ int main(int argc, char** argv) {
         switch (op) {
           case 0: operand = OPERAND_ADD; break;
           case 1: assert(false && "(not used)");
-          case 2: assert(false && "TODO: adc");
-          case 3: assert(false && "TODO: sbb");
+          case 2: 
+            error = STRING_LIT("TODO: adc");
+            break;
+          case 3: 
+            error = STRING_LIT("TODO: sbb");
+            break;
           case 4: assert(false && "(not used)");
           case 5: operand = OPERAND_SUB; break;
           case 6: assert(false && "(not used)");
           case 7: operand = OPERAND_CMP; break;
+
           default: assert(false);
         }
         dest = register_memory_name;
@@ -525,7 +621,7 @@ int main(int argc, char** argv) {
         uint16_t data = next_data_u16(&instruction_stream, w);
 
         operand = OPERAND_MOV;
-        dest    = register_names[b1 - 0xB0 + 1];
+        dest    = register_names[b1 - 0xB1];
         source  = string_pushf("%d", data);
       } break;
 
@@ -548,7 +644,6 @@ int main(int argc, char** argv) {
       } break;
 
       // INC, DEC, CALL id, CALL l id, JMP id, PUSH, -
-      /*
       case 0xFF: // Grp 2 w,r/m
       {
         uint8_t b2 = next_byte(&instruction_stream);
@@ -556,20 +651,26 @@ int main(int argc, char** argv) {
         uint8_t op  = (b2 >> 3) & 7;
         uint8_t rm  = (b2 >> 0) & 7;
 
-        error = string_pushf("TODO: ");
-
         switch (op) {
-          case 0: break; // INC  MEM16
-          case 1: break; // DEC  MEM16
-          case 2: break; // CALL REG16/MEM16 (intra)
-          case 3: break; // CALL MEM16 (intersegment)
-          case 4: break; // JMP  REG16/MEM16 (intersegment)
-          case 6: break; // PUSH MEM16
-          case 7: assert(false); break; // (not used)
+          case 6: // PUSH MEM16
+            operand = OPERAND_PUSH;
+            string_t address = next_effective_address(&instruction_stream, mod, rm);
+            dest = string_pushf("word %.*s", STRING_FMT(address));
+            break;
+
+          // case 0: break; // INC  MEM16
+          // case 1: break; // DEC  MEM16
+          // case 2: break; // CALL REG16/MEM16 (intra)
+          // case 3: break; // CALL MEM16 (intersegment)
+          // case 4: break; // JMP  REG16/MEM16 (intersegment)
+          // case 7: assert(false); break; // (not used)
+
+          default: 
+            error = string_pushf("Unexpected op: 0b%.*s", 3, &bit_string_u8(op)[6]);
+            break;
         }
 
       } break;
-      */
 
       default:
         error = string_pushf("Unexpected instruction 0b%s", bit_string_u8(b1));
